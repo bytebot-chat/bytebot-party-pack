2b1fcb017f32292513d8b0a3d2e684a79edfefd4
parsec <beckmanseth@gmail.com>
2021-07-07 20:53:31 -0400

    clarified comment

--- decisions.go
+++ decisions.go
@@ -7,7 +7,7 @@ import (
 
 func choose(nick string, msg string) string {
 
-	choices := strings.Split(msg, "or") // declaring the choices array and splitting the msg content into parts
+	choices := strings.Split(msg, "or") // declaring the choices array and splitting the msg content into parts for parsing
 
 	choice := choices[rand.Intn(len(choices)-1)] // using rand to select a random choice
 
