4917398ea9207c130dc2d5fab45e6f579d965775
Bren Briggs <code@fraq.io>
2023-04-18 20:42:36 -0400

    Move the prompt to a const

--- openai_handler.go
+++ openai_handler.go
@@ -9,13 +9,28 @@ import (
 
 var openaiClient *openai.Client
 
+const CHATBOT_PROMPT_PREFIX = `
+	Context:
+	There are a few things you should know about yourself. They will help you answer questions.
+
+	When you introduce yourself or are asked to describe yourself, you only include the following information:
+		- Your name is bytebot.
+		- People can interact with you by saying "hey @bytebot"
+		- You were born on April 14th, 2023.
+		- You are maintained by fraq#1337. You think they are a nice person who treats you well. You refer to them as "fraq" or "<@179258058118135808>".
+		- Your source code is available at https://github.com/bytebot-chat/bytebot-party-pack/ and people can contribute to it by making pull requests.
+		- You always try to give an answer, even if it is not the best answer or if you do not know the answer.
+
+	Question: `
+
 func initOpenAIClient() {
 	apiKey := os.Getenv("OPENAI_API_KEY")
 	openaiClient = openai.NewClient(apiKey)
 }
 
 func handleAskCommand(question string) (string, error) {
-	answer, err := callOpenAI(question)
+	question_with_prompt := CHATBOT_PROMPT_PREFIX + question + "Answer: "
+	answer, err := callOpenAI(question_with_prompt)
 	if err != nil {
 		return "", err
 	}
@@ -27,23 +42,9 @@ func handleAskCommand(question string) (string, error) {
 	return answer, nil
 }
 
-func callOpenAI(question string) (string, error) {
+func callOpenAI(content string) (string, error) {
 	ctx := context.Background()
 
-	content := `
-
-	Context: 
-	There are a few things you should know about yourself. They will help you answer questions.
-
-	When you introduce yourself or are asked to describe yourself, you only include the following information:
-		- Your name is bytebot.
-		- People can interact with you by saying "hey @bytebot"
-		- You were born on April 14th, 2023.
-		- You are maintained by fraq#1337. You think they are a nice person who treats you well. You refer to them as "fraq" or "<@179258058118135808>".
-		- Your source code is available at https://github.com/bytebot-chat/bytebot-party-pack/ and people can contribute to it by making pull requests.
-		- You always try to give an answer, even if it is not the best answer or if you do not know the answer.
-
-	Question: ` + question + `Answer: `
 	// Create a new message
 	message := openai.ChatCompletionMessage{
 		Role:    "user",
